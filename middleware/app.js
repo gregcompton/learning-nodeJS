df99efeeb467a46156612f3cf9dedd4c8b134335
gregcompton <greg@gregcompton.com>
2019-09-02 10:36:48 -0400

    finished MiddleWare

---
+++ middleware/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use((req,res,next)=>{
+  //console.log(req.url,req.method);
+  req.banana = 'not banana'
+  next();
+});
+
+app.get('/',(req,res)=>{
+  console.log(req.banana);
+  res.send('MiddleWare');
+});
+
+app.listen(3000);
