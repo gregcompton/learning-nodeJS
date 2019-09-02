55f52820f148d719fc403c27629e28ef23388db3
gregcompton <greg@gregcompton.com>
2019-09-02 00:21:36 -0400

    finished json data express bodyparser

---
+++ http_post_request_express/app.js
@@ -0,0 +1,24 @@
+// noob reminders:
+// remember to npm install express
+// remember to npm init --yes to create package.json
+// this example requires npm install body-parser
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({extended: false})); // this allows us to parse URL encoded forms
+
+app.get('/', (req,res)=>{
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.post('/', (req,res)=>{
+  console.log(req.body);
+  // do some database work here
+  res.send('successfully posted data');
+});
+
+app.listen(3000);
